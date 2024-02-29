d555231a112f25de6b1d9022e7dd74314b9f51da
tktharani <tktharani@gmail.com>
2024-02-29 14:28:10 +0530

    first message

---
+++ myproject/src/main/java/com/pro/myproject/Myproject1Application.java
@@ -0,0 +1,13 @@
+package com.pro.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject1Application.class, args);
+	}
+
+}
---
+++ myproject/src/test/java/com/pro/myproject/Myproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
